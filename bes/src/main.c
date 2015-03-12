5e730ed912f92ad682db876193bef49b1f302097
Nuno Anselmo <n.anselmo@hotmail.com>
2015-03-12 20:36:04 +0000

    Prepared new_dynamic for merge with master

--- bes/src/main.c
+++ bes/src/main.c
@@ -1,6 +1,9 @@
 #include "includes.h"
 #include "main.h"
 
+#define QUIT 11
+#define CONTINUE 17
+
 /*
 void addBank(char *nome, char rating, int reference){
 	Bank bank;
@@ -39,10 +42,76 @@ int partners(Bank* bank){
 }
 */
 
+int requestInput()
+{
+	switch(getchar())
+	{
+		case 'a':
+		{
+			char name[40];
+			int rating;
+			int reference;
+			scanf("%s %d %d", name, &rating, &reference);
+			break;
+		}
+		case 'k':
+		{
+			int reference;
+			scanf("%d", &reference);
+			break;
+		}
+		case 'r':
+		{
+			int reference;
+			scanf("%d", &reference);
+			break;
+		}
+		case 'e':
+		{
+			int reference1;
+			int reference2;
+			int amount;
+			scanf("%d %d %d", &reference1, &reference2, &amount);
+			break;
+		}
+		case 'p':
+		{
+			int reference1;
+			int reference2;
+			int amount;
+			scanf("%d %d %d", &reference1, &reference2, &amount);
+			break;
+		}
+		case 'l':
+		{
+			int type;
+			scanf("%d", &type);
+			break;
+		}
+		case 'K':
+		{
+			break;
+		}
+		case 'x':
+		{
+			return QUIT;
+		}
+		default:
+		{
+			printf("ERROR! Unhandled user input! Aborting!");
+			return QUIT;
+		}
+	}
+	return CONTINUE;
+}
+
 int main(int argc, char const *argv[]){
 	Network *network;
 	network = malloc(sizeof(Network));
 	network_init(network);
+	
+	/*while (requestInput() != QUIT){}*/
+	
 	network_addBank(network, "ola", 1, 18273);
 	network_addBank(network, "stuff", 1, 19281);
 	network_addBank(network, "coisas", 1, 1647);

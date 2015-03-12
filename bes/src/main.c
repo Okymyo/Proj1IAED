51d70335c1598a34b518759a81077457a34d86c0
Nuno Anselmo <n.anselmo@hotmail.com>
2015-03-12 20:49:09 +0000

    Removed obsolete commented functions

--- bes/src/main.c
+++ bes/src/main.c
@@ -4,44 +4,6 @@
 #define QUIT 11
 #define CONTINUE 17
 
-/*
-void addBank(char *nome, char rating, int reference){
-	Bank bank;
-	bank_init(&bank, nome, rating, reference);
-	banks[banksNum] = bank;
-	banksNum++;	
-}
-
-Bank* bankByReference(int reference){
-	int i;
-	for (i = 0; i < banksNum; i++){
-		Bank *bank = &banks[i];
-		if(bank_reference(bank) == reference){
-			return bank;
-		}	
-	}
-	return NULL;
-}
-
-int partners(Bank *bank){
-	int i, j, total = 0;
-	for (i = 0; i < banksNum; i++){
-		Bank *currentBank = &banks[i];
-		for (j = 0; j < bank_loansNum(currentBank); j++){
-			Loan *currentLoan = bank_loan(currentBank, j);
-			if(loan_reference(currentLoan) == bank_reference(bank)){
-				if(bank_loanByReference(bank, bank_reference(currentBank)) 
-					== NULL){
-					total++;
-				}
-			}
-		}
-	}
-	total+=bank_loansNum(bank);
-	return total;
-}
-*/
-
 int requestInput()
 {
 	switch(getchar())
